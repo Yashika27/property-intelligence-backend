724b79787580bf035e03e5d041e477742915771c
yashika.lamba <yashika.lamba@hilabs.com>
2025-06-13 19:12:02 +0530

    added logger on insertion, modified googleSheetsService

--- src/main/java/com/example/demo/service/GoogleSheetsService.java
+++ src/main/java/com/example/demo/service/GoogleSheetsService.java
@@ -8,13 +8,17 @@ import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 import java.util.List;
+import java.util.Objects;
+import java.util.UUID;
 
+@Slf4j
 @Service
 public class GoogleSheetsService {
 
@@ -26,9 +30,22 @@ public class GoogleSheetsService {
         sheetsService = getSheetsService();
     }
 
+//    private Sheets getSheetsService() throws IOException, GeneralSecurityException {
+//        GoogleCredentials credentials = GoogleCredentials.fromStream(
+//                        new FileInputStream("src/main/resources/credentials.json"))
+//                .createScoped(List.of("https://www.googleapis.com/auth/spreadsheets"));
+//
+//        return new Sheets.Builder(
+//                GoogleNetHttpTransport.newTrustedTransport(),
+//                JacksonFactory.getDefaultInstance(),
+//                new HttpCredentialsAdapter(credentials))
+//                .setApplicationName("My App")
+//                .build();
+//    }
+
     private Sheets getSheetsService() throws IOException, GeneralSecurityException {
         GoogleCredentials credentials = GoogleCredentials.fromStream(
-                        new FileInputStream("src/main/resources/credentials.json"))
+                        Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("credentials.json")))
                 .createScoped(List.of("https://www.googleapis.com/auth/spreadsheets"));
 
         return new Sheets.Builder(
@@ -42,6 +59,7 @@ public class GoogleSheetsService {
     public void addRow(ContactFormDTO contactFormDTO) throws IOException {
         ValueRange body = new ValueRange()
                 .setValues(List.of(List.of(
+                        UUID.randomUUID(),
                         contactFormDTO.getName(),
                         contactFormDTO.getEmail(),
                         contactFormDTO.getPhone(),
@@ -49,6 +67,8 @@ public class GoogleSheetsService {
                         contactFormDTO.getSubject(),
                         contactFormDTO.getMessage()
                 )));
+
+        log.info("Entering query by user {}", contactFormDTO.getName());
 
         sheetsService.spreadsheets().values()
                 .append(SPREADSHEET_ID, "Sheet1", body)
