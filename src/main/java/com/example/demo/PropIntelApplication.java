8617ddabe6257650664ddc04d9ae5824cfed9089
yashika.lamba <yashika.lamba@hilabs.com>
2025-06-20 10:21:57 +0530

    default added

--- src/main/java/com/example/demo/PropIntelApplication.java
+++ src/main/java/com/example/demo/PropIntelApplication.java
@@ -3,13 +3,26 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class PropIntelApplication {
 
 	public static void main(String[] args) {
+//		String port = System.getenv("PORT");
+//		System.out.println("ENV PORT: " + port);
+//		SpringApplication.run(PropIntelApplication.class, args);
+
+		SpringApplication app = new SpringApplication(PropIntelApplication.class);
+		Map<String, Object> props = new HashMap<>();
 		String port = System.getenv("PORT");
-		System.out.println("ENV PORT: " + port);
-		SpringApplication.run(PropIntelApplication.class, args);
+		if (port != null) {
+			props.put("server.port", port);
+		}
+		props.put("server.address", "0.0.0.0");
+		app.setDefaultProperties(props);
+		app.run(args);
 	}
 
 }
